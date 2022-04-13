112e0b4c948f0d6c28bb32eba857bd06a2745f77
ShubhamDhanda <shubhamdhanda944@gmail.com>
2022-04-13 10:33:09 +0530

    music app updated addition

---
+++ BackEnd/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "";
+// const uri = "mongodb+srv://bhannasa:MU%23TzgFh%247pvj8q@cluster0.b1ntj.mongodb.net/test";
+// const uri = "mongodb+srv://bhannasa:MU%23TzgFh%247pvj8q@cluster0.b1ntj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+mongoose.connect(uri).then(() => {
+    console.log("Connected")
+}).catch((err) => {
+    //console.log(err);
+});
+
+console.log(__dirname);
+
+
+
+app.use("/api/user",require('./routes/user'));
+app.use("/api/song",require('./routes/song'));
+app.use("/api/playlist",require('./routes/playlist'));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+/*
+app.get('/audio', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/playlist', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});*/
+
+
+app.listen(port, ()=>{
+    console.log("Listening to port 5000")
+})
